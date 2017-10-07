2b32c95ab3222d27149fee7ae841f159d2fff0de
Denny Hung <dennyhl@berkeley.edu>
2017-10-07 00:50:16 -0700

    Added socke.io implementation for the bot's server

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ const server = app.listen(5000);
 app.get('/', (req, res) => {
 	res.sendFile('index.html');
 )};
+
+const APIAI_TOKEN = ''
+const APIAI_SESSION_ID = ''
+const apiai = require('apiai')(APIAI_TOKEN);
+
+io.on('connection', function(socket) {
+	socket.on('chat message', (text) => {
+
+		let apiaiReq = apiai.textRequest(text, {
+			sessionId: APIAI_SESSION_ID
+		});
+
+		apiaiReq.on('response', (response) => {
+			let aiText = response.result.fulfillment.speech;
+			socket.emit('bot reply', aiText);
+		});
+
+		apiaiReq.on('error', (error) => {
+			console.log(error);
+		});
+
+	});
+});
