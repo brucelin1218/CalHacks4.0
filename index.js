781569172f9eb0fca8dca6599050111e49d4f634
Bruce Lin <brucelin1218@gmail.com>
2017-10-06 23:36:54 -0700

    Add index.js, index.html and script.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-index.js
+const express = require('express')
+const app = express();
+
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // js, css, images
+
+const server = app.listen(5000);
+app.get('/', (req, res) => {
+	res.sendFile('index.html');
+)};
