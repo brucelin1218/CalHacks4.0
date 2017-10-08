f550a2222617e91a2d5c27870ec39ebb5547a8f7
Carson Trinh <trinh.carson@berkeley.edu>
2017-10-07 21:31:53 -0700

    Working topic recent news summarizer

--- news_shortener/news.py
+++ news_shortener/news.py
@@ -1,6 +1,21 @@
+# For suny
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+
 from bs4 import BeautifulSoup
 import requests
 import re
+
+#For suny
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+LANGUAGE = "english"
+SENTENCES_COUNT = 3
 
 def search(search_phrase):
 	page = requests.get("https://news.google.com/news/search/section/q/" + search_phrase)
@@ -8,20 +23,43 @@ def search(search_phrase):
 	soup = BeautifulSoup(page.content, "lxml")
 
 	link_start = 26
-	'''
+	"""
 	for a in soup.find_all('a')[26:35]:
 		print("Link #" + str(i) + "\n")
 		print(str(a) + "\n")
 		i += 1
-	'''
+	"""
 	href = soup.find_all('a')[link_start].get('href')
 	# TODO If href is redirected to google.com, end the program
 	if href.startswith("https://www.google.com"):
 		return False
 	print(href)
+	return href
 
-def main():
-	search_phrase = input("What would you like to news about? ")
-	search(search_phrase)
+def summarize(url):
+	result = ""
+	if __name__ == "__main__":
+	    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+	    # or for plain text files
+	    # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
+	    stemmer = Stemmer(LANGUAGE)
+
+	    summarizer = Summarizer(stemmer)
+	    summarizer.stop_words = get_stop_words(LANGUAGE)
+
+	    for sentence in summarizer(parser.document, SENTENCES_COUNT):
+	        result += str(sentence) + " "
+	return result
+
+def main(search_phrase):
+	# search_phrase = input("What would you like to news about? ")
+	url = search(search_phrase)
+	summary = summarize(url)
+	# print(summary)
+	return summary
+
 while True:
-	main()
+	search_phrase = input("What would you like to news about? ")
+	if (search_phrase == "exit"):
+		exit()
+	main(search_phrase)
